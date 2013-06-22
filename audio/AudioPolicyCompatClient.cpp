LineageOS/android_hardware_libhardware_legacy

libhardware_legacy: don't always include setFmVolume
Change-Id: I26414abec0e82b0a6e2c22c14b8b16ffada2c2ec

audio/AudioPolicyCompatClient.cpp
@@ -140,7 +140,7 @@ status_t AudioPolicyCompatClient::setStreamVolume(
                                           volume, output, delayMs);
 }
 
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(FM_RADIO) && !defined(USES_AUDIO_LEGACY)
 status_t AudioPolicyCompatClient::setFmVolume(float volume,
                                               int delayMs)
 {

audio/AudioPolicyCompatClient.h
@@ -76,7 +76,7 @@ public:
     virtual status_t startTone(ToneGenerator::tone_type tone, AudioSystem::stream_type stream);
     virtual status_t stopTone();
     virtual status_t setVoiceVolume(float volume, int delayMs = 0);
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(FM_RADIO) && !defined(USES_AUDIO_LEGACY)
     virtual status_t setFmVolume(float volume, int delayMs = 0);
 #endif
 

include/hardware_legacy/AudioHardwareInterface.h
@@ -202,7 +202,7 @@ public:
     /** set the audio volume of a voice call. Range is between 0.0 and 1.0 */
     virtual status_t    setVoiceVolume(float volume) = 0;
 
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(FM_RADIO) && !defined(USES_AUDIO_LEGACY)
     /** set the fm volume. Range is between 0.0 and 1.0 */
     virtual status_t    setFmVolume(float volume) { return 0; }
 #endif

include/hardware_legacy/AudioPolicyInterface.h
@@ -254,7 +254,7 @@ public:
                                      audio_io_handle_t srcOutput,
                                      audio_io_handle_t dstOutput) = 0;
 
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(FM_RADIO) && !defined(USES_AUDIO_LEGACY)
     // set FM volume.
     virtual status_t setFmVolume(float volume, int delayMs = 0) { return 0; }
 #endif

android-legacy-repos/platform_hardware_libhardware_legacy

audio/AudioPolicyCompatClient.cpp
@@ -140,7 +140,7 @@ status_t AudioPolicyCompatClient::setStreamVolume(
                                           volume, output, delayMs);
 }
 
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(HAVE_FM_RADIO) && !defined(USES_AUDIO_LEGACY)
 status_t AudioPolicyCompatClient::setFmVolume(float volume,
                                               int delayMs)
 {

audio/AudioPolicyCompatClient.h
@@ -76,7 +76,7 @@ public:
     virtual status_t startTone(ToneGenerator::tone_type tone, AudioSystem::stream_type stream);
     virtual status_t stopTone();
     virtual status_t setVoiceVolume(float volume, int delayMs = 0);
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(HAVE_FM_RADIO) && !defined(USES_AUDIO_LEGACY)
     virtual status_t setFmVolume(float volume, int delayMs = 0);
 #endif
 

include/hardware_legacy/AudioHardwareInterface.h
@@ -202,7 +202,7 @@ public:
     /** set the audio volume of a voice call. Range is between 0.0 and 1.0 */
     virtual status_t    setVoiceVolume(float volume) = 0;
 
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(HAVE_FM_RADIO) && !defined(USES_AUDIO_LEGACY)
     /** set the fm volume. Range is between 0.0 and 1.0 */
     virtual status_t    setFmVolume(float volume) { return 0; }
 #endif

include/hardware_legacy/AudioPolicyInterface.h
@@ -254,7 +254,7 @@ public:
                                      audio_io_handle_t srcOutput,
                                      audio_io_handle_t dstOutput) = 0;
 
-#if defined(QCOM_HARDWARE) && !defined(USES_AUDIO_LEGACY)
+#if defined(QCOM_HARDWARE) && defined(HAVE_FM_RADIO) && !defined(USES_AUDIO_LEGACY)
     // set FM volume.
     virtual status_t setFmVolume(float volume, int delayMs = 0) { return 0; }
 #endif